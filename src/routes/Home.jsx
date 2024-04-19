6488a7373093c05f5df62fe499a4369dd54b480a
giorgi kochuashvili <103053252+giorgio324@users.noreply.github.com>
2024-04-19 09:57:08 +0000

    done

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
+  const swap = (array) => {
+    for (let index = 0; index < array.length - 1; index += 2) {
+      if (index + 1 < array.length) {
+        const temp = array[index];
+        array[index] = array[index + 1];
+        array[index + 1] = temp;
+      }
+    }
+    return array;
+  };
+
+  useEffect(() => {
+    console.log(swap([1, 2, 3, 4, 2, 3]));
+  }, []);
   return (
     <div className='App'>
       <header className='App-header'>
         <Link to='list'>დაკლიკე და ჩაირიცხე</Link>
       </header>
     </div>
-  )
-}
+  );
+};
--- src/routes/List.jsx
+++ src/routes/List.jsx
@@ -1,5 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import data1 from '../data/Array_1.json';
+import data2 from '../data/Array_2.json';
 
 export default function List() {
-  return <div>list</div>
+  const [data, setData] = useState([]);
+  const putTogether = () => {
+    const combinedData = data1.concat(data2);
+    const uniqueData = [];
+
+    for (let i = 0; i < combinedData.length; i++) {
+      let isDuplicate = false;
+
+      for (let j = 0; j < uniqueData.length; j++) {
+        if (combinedData[i].id === uniqueData[j].id) {
+          isDuplicate = true;
+          break;
+        }
+      }
+
+      if (!isDuplicate) {
+        uniqueData.push(combinedData[i]);
+      }
+    }
+
+    setData(uniqueData);
+  };
+
+  useEffect(() => {
+    putTogether();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item) => {
+        return (
+          <>
+            <p>{item.body}</p>
+            <p>{item.name}</p>
+          </>
+        );
+      })}
+    </div>
+  );
 }
